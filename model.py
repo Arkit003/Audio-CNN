8875a8de509370a44f663957fcff0d363f2f9d00
Arkit <arkit312003@gmail.com>
2026-01-13 19:58:30 +0000

    model structure

---
+++ model.py
@@ -0,0 +1,69 @@
+import torch.nn as nn
+import torch
+
+class ResidualBlock(nn.Module):
+    def __init__(self,in_channels,out_channels,stride=1):
+        super().__init__()
+        self.conv1= nn.Conv2d(in_channels,out_channels,3,
+                              stride,padding=1,bias=False)#not adding bias as our batchnorm layer will gonna have it ,here it would be just waste of compute
+        self.bn1= nn.BatchNorm2d(out_channels)
+        self.conv2= nn.Conv2d(out_channels,out_channels,3,
+                              stride,padding=1,bias=False)  
+        self.bn2= nn.BatchNorm2d(out_channels)
+        self.shortcut=nn.Sequential()
+        self.use_shortcut= stride !=1 or in_channels != out_channels
+        if self.use_shortcut:
+            self.shortcut=nn.Sequential(
+                #for keeping dimension same we are using 1 kernel(same out put as tensor) in the shrotcut
+                nn.Conv2d(in_channels,out_channels,1,stride=stride,bias=False),
+                nn.BatchNorm2d(out_channels)
+            )
+            
+    def forward(self,x):
+        out=self.conv1(x)
+        out=self.bn1(out)
+        out=torch.relu(out)
+        out=self.conv2(out)
+        out=self.bn2(out)
+        out=torch.relu(out)
+        #if x is not dimesionally correct making it correct and then adding
+        shortcut=self.shortcut(x) if self.use_shortcut else x
+        out_add=out +shortcut
+        out=torch.relu(out_add)
+        return out        
+        
+class AudioCNN(nn.Module):
+    def __init__(self,num_classes=50):
+        super().__init__()
+
+        self.conv1=nn.Sequential(
+            nn.Conv2d(in_channels=1,out_channels=64,kernel_size=7,padding=3,stride=2,bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),#inplace true makes it applied on the last layer
+            nn.MaxPool2d(3,2,1)
+            )
+        self.layer1=nn.ModuleList([ResidualBlock(64,64) for _ in range(3)])#module list doesn't have a forward behaviour
+        
+        self.layer2=nn.ModuleList([ResidualBlock(64 if i==0 else 128, 128) for i in range(4)])
+        self.layer3=nn.ModuleList([ResidualBlock(128 if i==0 else 256, 256) for i in range(6)])
+        self.layer4=nn.ModuleList([ResidualBlock(256 if i==0 else 512, 512) for i in range(3)])
+        self.avgpool=nn.AdaptiveAvgPool2d((1,2))
+        self.dropout=nn.Dropout(0.5)
+        self.fullyconnected=nn.Linear(512,num_classes)        
+        
+    def forward(self,x):
+        x=self.conv1(x)
+        for block in self.layer1:
+            x=block(x)
+        for block in self.layer2:
+            x=block(x)
+        for block in self.layer3:
+            x=block(x)
+        for block in self.layer4:
+            x=block(x)
+        x=self.avgpool(x)
+        x=x.view(x.size(0),-1)#flattening from (B,512,1,1) to (B,512)
+        x=self.dropout(x)
+        x=self.fullyconnected(x)
+        
+        return x
